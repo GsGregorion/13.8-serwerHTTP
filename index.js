46d1e24052407db1083a0826090714eeb6abc438
Gregorion Skquarna <gs.gregorion@gmail.com>
2018-10-24 12:16:22 +0200

    Correct accordig to mentor remarks

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    
     if (request.method === 'GET' && request.url === '/correctUrl') {
         fs.readFile('./index.html', (err, data) => {
             if (err) throw err;
+            response.setHeader("Content-Type", "text/html; charset=utf-8");
             response.write(data);
             response.end();
         });
 
     }   else {
-            response.write('<body><img src="https://bubblefootballzone.co.uk/wp-content/uploads/2014/03/3-ways-to-create-more-engaging-404-pages2.jpg"</body>')
-            response.end();
+            fs.readFile('./error.jpg', (err, data) => {
+                if (err) throw err;
+                response.writeHead(200, {'Content-Type': 'image/jpeg'});
+                response.write(data);
+                response.end();
+            });
         }
 });
 
